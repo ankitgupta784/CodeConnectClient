82df185846f1c37571d95f5dfe14fc82d0baa8c8
Ankit Gupta <ankit.gupta8@dell.com>
2024-11-21 02:28:56 +0530

    update BaseURL

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,1 +1,2 @@
-export const BASE_URL = "https://code-connect-server.vercel.app";
+//export const BASE_URL = "http://localhost:7777";
+export const BASE_URL = "https://codeconnectserver.onrender.com";
